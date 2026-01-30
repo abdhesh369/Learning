888d56255fcd9ce001a80d8740597fbea3060605
Abdhesh <abdheshshah111@gmail.com>
2026-01-30 22:52:30 +0545

    Add fruits template and update app routes

--- FLASK/My First Flask App/app.py
+++ FLASK/My First Flask App/app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    user_name = "Abdhesh"
-    return render_template('home.html', name=user_name)
+# @app.route('/')
+# def home():
+#     user_name = "Abdhesh"
+#     return render_template('home.html', name=user_name)
+
+
+@app.route('/fruits')
+def fruits():
+    fruit_list = ["Apple", "Banana", "Mango", "Orange"]  # Python list (you know this!)
+    return render_template('fruits.html', fruits=fruit_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
